af2793c1eb354815fa3bfc2af06afc152d735668
yusiang <10656010@ntub.edu.tw>
2020-10-31 17:52:34 +0800

    新增 - 首頁架構

--- src/module/pages/nav.js
+++ src/module/pages/nav.js
@@ -3,18 +3,30 @@ import { Row, Col, Container } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Nav extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            "login": !!localStorage.getItem("login")
+        }
+    }
     render() {
         return (<Container>
             <Row className="justify-content-center">
+                {/* <Col sm={ "auto" }>
+                    <a href="./#/">首頁</a>
+                </Col> */}
                 <Col sm={ "auto" }>
-                    <a href="./#/sign">註冊</a>
-                </Col>
-                <Col sm={ "auto" }>
-                    <a href="./">議題大小事</a></Col>
+                    <a href="./#/">議題大小事</a></Col>
                 <Col sm={ "auto" }>政策專區</Col>
                 <Col sm={ "auto" }>人物專區</Col>
                 <Col sm={ "auto" }>進度專區</Col>
-                <Col sm={"auto"}><a href="./#/login">登入</a></Col>
+                { (this.state.login || false ? (<>
+                    <Col sm={ "auto" }>個人檔案</Col><Col sm={ "auto" }>登出</Col>
+                </>) : (<>
+                    <Col sm={ "auto" }><a href="./#/login">登入</a></Col>
+                    <Col sm={ "auto" }><a href="./#/sign">註冊</a></Col>
+                </>)) }
+
             </Row>
         </Container>)
     }
