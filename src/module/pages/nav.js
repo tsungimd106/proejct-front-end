5f1997490084751adbbeabba197078638e01770a
Vanessa <10656001@ntub.edu.tw>
2021-02-20 16:49:11 +0800

    調整 - 顏色&logo&navbar

--- src/module/pages/nav.js
+++ src/module/pages/nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Row, Col, Container ,Button} from "react-bootstrap"
-import logo_dark from '../../imgs/LOGO1.png'
-import search from '../../imgs/search.png'
+import { Row, Col, Container, Button, Dropdown,  } from "react-bootstrap"
+import logo_dark from '../../imgs/LOGO1.jpg'
+import { Search, Person, CircleChevronDown, SignOut } from 'akar-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../../css/nav.css"
+
 
 export default class Nav extends React.Component {
     constructor(props) {
@@ -12,9 +13,9 @@ export default class Nav extends React.Component {
             "login": !!localStorage.getItem("login")
         }
     }
-    logout=()=>{
+    logout = () => {
         localStorage.clear()
-        this.setState({"login":false})
+        this.setState({ "login": false })
         window.location.href = "./"
 
     }
@@ -24,22 +25,38 @@ export default class Nav extends React.Component {
                 <img className="navPicture" src={logo_dark}></img>
             </div>
             <Row className="justify-content-center">
-              
-                <Col sm={ "auto" }>
-                    <a href="./#/">議題大小事</a></Col>
-                <Col sm={ "auto" }>
+
+                <Col sm={"auto"}>
+                    <a href="./#/">選舉專區</a></Col>
+                <Col sm={"auto"}>
                     <a href="./#/policy/">提案專區</a></Col>
-                <Col sm={ "auto" }>
+                <Col sm={"auto"}>
                     <a href="./#/figure/">人物專區</a></Col>
-                { (this.state.login || false ? (<>
-                    <Col sm={ "auto" }>個人檔案</Col><Col sm={ "auto" }>
-                    <Button onClick={this.logout}>登出</Button></Col>
+                <Col sm={"auto"}>
+                    <input className="searchbar" type="text" name="搜尋"></input>
+                    <Search /></Col>
+
+                {(this.state.login || true ? (<>
+                    <Col sm={"auto"}><Person /></Col>
+                    <Col sm={"auto"}>
+                    <Dropdown>
+                        <Dropdown.Toggle  id="dropdown-basic">
+                            <CircleChevronDown />
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                        
+                    <SignOut onClick={this.logout} /></Col>
                 </>) : (<>
-                    <Col sm={ "auto" }><a href="./#/login">登入</a></Col>
-                    <Col sm={ "auto" }><a href="./#/sign">註冊</a></Col>
-                </>)) }
-                <Col sm={ "auto" }><input className="searchbar" type="text" name="搜尋"></input>
-                <img className="searchimg" src={search}></img></Col>
+                    <Col sm={"auto"}><a href="./#/login">登入</a></Col>
+                    <Col sm={"auto"}><a href="./#/sign">註冊</a></Col>
+                </>))}
+
 
             </Row>
         </Container>)
