8343bb1f7ba1c6fe0aa4bf5b0837c436a43464e3
Vanessa <10656001@ntub.edu.tw>
2021-04-01 18:42:36 +0800

    調整 - 首頁排版

--- src/module/pages/home.js
+++ src/module/pages/home.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Row, Col, Carousel, Card, Button, } from "react-bootstrap"
+import { Row, Col, Carousel, Card, Button, ListGroup, } from "react-bootstrap"
 import { Pages } from "../pages.js"
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
+import person from "../../imgs/person.png"
+
 // import "../../css/main.css"
 import style from "../../css/main.module.css"
 
@@ -19,6 +21,55 @@ class Home extends React.Component {
                 "https://i1.kknews.cc/SIG=rf6m48/31p9000467p4po554154.jpg"
 
             ],
+            chart: [
+                { name: '王婉諭', score: 72, },
+                { name: '賴品妤', score: 98 },
+                { name: '蔡適應', score: 52 },
+                { name: '林昶佐', score: 66 },
+            ],
+            proposal:
+            {
+                hot: [
+                    { name: '王婉諭', title: "公民投票法部分條文修正草案" },
+                    { name: '劉建國', title: "太空發展法訂定草案" },
+                    { name: '郭家瑜', title: "新住民孩童教育制定草案" },
+                ],
+                new: [
+                    { name: '王婉諭', title: "公民投票法部分條文修正草案" },
+                    { name: '劉建國', title: "太空發展法訂定草案" },
+                    { name: '郭家瑜', title: "新住民孩童教育制定草案" },
+                ]
+            }
+            ,
+            message:
+            {
+                hot: [
+                    { name: '王婉諭', title: "公民投票法部分條文修正草案" },
+                    { name: '劉建國', title: "太空發展法訂定草案" },
+                    { name: '郭家瑜', title: "新住民孩童教育制定草案" },
+                ],
+                new: [
+                    { name: '王婉諭', title: "公民投票法部分條文修正草案" },
+                    { name: '劉建國', title: "太空發展法訂定草案" },
+                    { name: '郭家瑜', title: "新住民孩童教育制定草案" },
+                ]
+            }
+            ,
+            politics:
+            {
+                hot: [
+                    { name: '王婉諭', title: "公民投票法部分條文修正草案" },
+                    { name: '劉建國', title: "太空發展法訂定草案" },
+                    { name: '郭家瑜', title: "新住民孩童教育制定草案" },
+                ],
+                new: [
+                    { name: '王婉諭', title: "公民投票法部分條文修正草案" },
+                    { name: '劉建國', title: "太空發展法訂定草案" },
+                    { name: '郭家瑜', title: "新住民孩童教育制定草案" },
+                ]
+            }
+            ,
+
         }
     }
 
@@ -50,51 +101,194 @@ class Home extends React.Component {
 
                     </Row>
                 </div>
-                <div className={style.width85}>政治人物排行榜
+
+                <div className={style.title}>政治人物排行榜
                     <Row>
-                        <Col>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col>
-                        <   Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+                        {this.state.chart || false ? (<>
+                            {this.state.chart.map(placement => {
+                                return (
+                                    <Col sm={"3"}>
+                                        <Card className={style.pimg} >
+                                            <Card.Img variant="top" src={person} className={style.pimg} />
+                                            <Card.Body>
+                                                <Card.Title>{placement.name}</Card.Title>
+                                                <Card.Text>
+                                                    我的分數：{placement.score}
+                                                </Card.Text>
+                                                <Button variant="primary">我的數據儀表板</Button>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>)
+                            })}
+                        </>) : (<></>)}
                     </Row>
                 </div>
-                <div>最新排行榜</div>
-                <div>最熱門排行榜</div>
+                <Row className={style.width85}>
+                    <Col>
+                        <Card>
+                            <Card.Body>最新提案：
+                                <ListGroup variant="flush">
+                                    {this.state.proposal || false ? (<>
+                                        {this.state.proposal.new.map(placement => {
+                                            return (
+                                                <ListGroup.Item>
+                                                    <Row className="align-items-center" noGutters={true}>
+                                                        <Col sm={3}>
+                                                            <Row className="justify-content-center">
+                                                                <Col sm={7}><img src={person} className="" /></Col>
+                                                                <Col sm={12}><div className={style.msg_name}>{placement.name}</div></Col>
+                                                            </Row>
+
+                                                        </Col>
+                                                        <Col sm={8}>{placement.title}</Col>
+                                                    </Row>
+                                                </ListGroup.Item>
+                                            )
+                                        })}
+                                    </>) : (<></>)}
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card >
+                            <Card.Body>最熱門提案：
+                                <ListGroup variant="flush">
+                                    {this.state.proposal || false ? (<>
+                                        {this.state.proposal.hot.map(placement => {
+                                            return (
+                                                <ListGroup.Item>
+                                                    <Row className="align-items-center" noGutters={true}>
+                                                        <Col sm={3}>
+                                                            <Row className="justify-content-center">
+                                                                <Col sm={7}><img src={person} className="" /></Col>
+                                                                <Col sm={12}><div className={style.msg_name}>{placement.name}</div></Col>
+                                                            </Row>
+
+                                                        </Col>
+                                                        <Col sm={8}>{placement.title}</Col>
+                                                    </Row>
+                                                </ListGroup.Item>
+                                            )
+                                        })}
+                                    </>) : (<></>)}
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
+
+                <Row className={style.width85}>
+                    <Col>
+                        <Card>
+                            <Card.Body>最新留言：
+                                <ListGroup variant="flush">
+                                    {this.state.message || false ? (<>
+                                        {this.state.message.new.map(placement => {
+                                            return (
+                                                <ListGroup.Item>
+                                                    <Row className="align-items-center" noGutters={true}>
+                                                        <Col sm={3}>
+                                                            <Row className="justify-content-center">
+                                                                <Col sm={7}><img src={person} className="" /></Col>
+                                                                <Col sm={12}><div className={style.msg_name}>{placement.name}</div></Col>
+                                                            </Row>
+
+                                                        </Col>
+                                                        <Col sm={8}>{placement.title}</Col>
+                                                    </Row>
+                                                </ListGroup.Item>
+                                            )
+                                        })}
+                                    </>) : (<></>)}
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card >
+                            <Card.Body>最熱門留言：
+                                <ListGroup variant="flush">
+                                    {this.state.message || false ? (<>
+                                        {this.state.message.hot.map(placement => {
+                                            return (
+                                                <ListGroup.Item>
+                                                    <Row className="align-items-center" noGutters={true}>
+                                                        <Col sm={3}>
+                                                            <Row className="justify-content-center">
+                                                                <Col sm={7}><img src={person} className="" /></Col>
+                                                                <Col sm={12}><div className={style.msg_name}>{placement.name}</div></Col>
+                                                            </Row>
+
+                                                        </Col>
+                                                        <Col sm={8}>{placement.title}</Col>
+                                                    </Row>
+                                                </ListGroup.Item>
+                                            )
+                                        })}
+                                    </>) : (<></>)}
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
+                <Row className={style.width85}>
+                    <Col>
+                        <Card>
+                            <Card.Body>最新政見：
+                                <ListGroup variant="flush">
+                                    {this.state.politics || false ? (<>
+                                        {this.state.politics.new.map(placement => {
+                                            return (
+                                                <ListGroup.Item>
+                                                    <Row className="align-items-center" noGutters={true}>
+                                                        <Col sm={3}>
+                                                            <Row className="justify-content-center">
+                                                                <Col sm={7}><img src={person} className="" /></Col>
+                                                                <Col sm={12}><div className={style.msg_name}>{placement.name}</div></Col>
+                                                            </Row>
+
+                                                        </Col>
+                                                        <Col sm={8}>{placement.title}</Col>
+                                                    </Row>
+                                                </ListGroup.Item>
+                                            )
+                                        })}
+                                    </>) : (<></>)}
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card >
+                            <Card.Body>最熱門政見：
+                                <ListGroup variant="flush">
+                                    {this.state.politics || false ? (<>
+                                        {this.state.politics.hot.map(placement => {
+                                            return (
+                                                <ListGroup.Item>
+                                                    <Row className="align-items-center" noGutters={true}>
+                                                        <Col sm={3}>
+                                                            <Row className="justify-content-center">
+                                                                <Col sm={7}><img src={person} className="" /></Col>
+                                                                <Col sm={12}><div className={style.msg_name}>{placement.name}</div></Col>
+                                                            </Row>
+
+                                                        </Col>
+                                                        <Col sm={8}>{placement.title}</Col>
+                                                    </Row>
+                                                </ListGroup.Item>
+                                            )
+                                        })}
+                                    </>) : (<></>)}
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
 
             </>)
         } />)
