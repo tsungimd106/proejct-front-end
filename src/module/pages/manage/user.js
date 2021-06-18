1e622b6e8707ce5f4dec532309988c151984b7c8
yusiang <10656010@ntub.edu.tw>
2021-06-18 08:47:12 +0800

    修改 - 管理者畫面

--- src/module/pages/manage/user.js
+++ src/module/pages/manage/user.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react'
- import style from "../../../css/manage.module.css"
+import style from "../../../css/manage.module.css"
 import { ManageR } from "../../request/manageR"
 
 import { Tab as TabUI, Button as BtnUI } from 'semantic-ui-react'
@@ -25,67 +25,69 @@ export default class Check extends React.Component {
         const panes = [
             {
                 menuItem: '一般使用者', render: () => <TabUI.Pane>
-                   <Grid>
-                    <Grid.Row className={ style.reportBox + "  " }>
-                        <Grid.Column width={ 4 }><h3>帳號</h3></Grid.Column>
-                        <Grid.Column width={ 4 }><h3>姓名</h3></Grid.Column>
-                        <Grid.Column width={ 8 }><h3>操作</h3></Grid.Column>
-                        
+                    <Grid>
+                        <Grid.Row className={ style.reportBox + "  " }>
+                            <Grid.Column width={ 4 }><h3>帳號</h3></Grid.Column>
+                            <Grid.Column width={ 4 }><h3>姓名</h3></Grid.Column>
+                            <Grid.Column width={ 8 }><h3>操作</h3></Grid.Column>
+                        </Grid.Row>
+                      
+                            { this.state.user && this.state.user.map((item, index) => {
+                                return (<>
+                                  <Grid.Row> 
+                                    <Grid.Column width={ 4 } >{ item.id }</Grid.Column>
+                                    <Grid.Column width={ 4 }>{ item.name }</Grid.Column>
+                                    <Grid.Column width={ 8 }>
+                                        <BtnUI>轉管理者</BtnUI>
+                                        <BtnUI>轉政治人物</BtnUI>
+                                    </Grid.Column> </Grid.Row>
 
-                        { this.state.user && this.state.user.map((item, index) => {
-                            return (<>
-                                <Grid.Column width={ 4 } >{ item.id }</Grid.Column>
-                                <Grid.Column width={ 4 }>{ item.name }</Grid.Column>
-                                <Grid.Column width={ 8 }>
-                                    <BtnUI>轉管理者</BtnUI>
-                                    <BtnUI>轉政治人物</BtnUI>
-                                </Grid.Column>
-                            
-                            </>)
-                        }) }
-                   </Grid.Row></Grid>  
+                                </>)
+                            }) }
+                       </Grid>
                 </TabUI.Pane>
             },
             {
                 menuItem: '政治人物使用者', render: () => <TabUI.Pane>
                     <Grid>
-                    <Grid.Row className={ style.reportBox + "  " }>
-                    <Grid.Column width={ 4 }><h3>帳號</h3></Grid.Column>
-                        <Grid.Column width={ 4 }><h3>姓名</h3></Grid.Column>
-                        <Grid.Column width={ 8 }><h3>操作</h3></Grid.Column>
+                        <Grid.Row className={ style.reportBox + "  " }>
+                            <Grid.Column width={ 4 }><h3>帳號</h3></Grid.Column>
+                            <Grid.Column width={ 4 }><h3>姓名</h3></Grid.Column>
+                            <Grid.Column width={ 8 }><h3>操作</h3></Grid.Column>
+                            </Grid.Row >
+                            { this.state.politician && this.state.politician.map((item, index) => {
+                                return (<> <Grid.Row> 
+                                    <Grid.Column width={ 4 } >{ item.id }</Grid.Column>
+                                    <Grid.Column width={ 4 }>{ item.name }</Grid.Column>
+                                    <Grid.Column width={ 8 }>
 
-                        { this.state.politician && this.state.politician.map((item, index) => {
-                            return (<>
-                                <Grid.Column width={ 4 } >{ item.id }</Grid.Column>
-                                <Grid.Column width={ 4 }>{ item.name }</Grid.Column>
-                                <Grid.Column width={ 8 }>
-                                   
-                                    <BtnUI>轉一般使用者</BtnUI>
-                                </Grid.Column>
-                            </>)
-                        }) }
-                    </Grid.Row ></Grid>
+                                        <BtnUI>轉一般使用者</BtnUI>
+                                    </Grid.Column>   </Grid.Row >
+                                </>)
+                            }) }
+                     </Grid>
                 </TabUI.Pane >
             },
             {
                 menuItem: '管理者', render: () => <TabUI.Pane>
                     <Grid>
-                     <Grid.Row className={ style.reportBox + "  " }>
-                    <Grid.Column width={ 4 }><h3>帳號</h3></Grid.Column>
-                        <Grid.Column width={ 4 }><h3>姓名</h3></Grid.Column>
-                        <Grid.Column width={ 8 }><h3>操作</h3></Grid.Column>
+                        <Grid.Row className={ style.reportBox + "  " }>
+                            <Grid.Column width={ 4 }><h3>帳號</h3></Grid.Column>
+                            <Grid.Column width={ 4 }><h3>姓名</h3></Grid.Column>
+                            <Grid.Column width={ 8 }><h3>操作</h3></Grid.Column>
+                            </Grid.Row >
+                            { this.state.manager && this.state.manager.map((item, index) => {
+                                return (<>
+                                  <Grid.Row>
+                                    <Grid.Column width={ 4 } >{ item.id }</Grid.Column>
+                                    <Grid.Column width={ 4 }>{ item.name }</Grid.Column>
+                                    <Grid.Column width={ 8 }>
 
-                        { this.state.manager && this.state.manager.map((item, index) => {
-                            return (<>
-                                <Grid.Column width={ 4 } >{ item.id }</Grid.Column>
-                                <Grid.Column width={ 4 }>{ item.name }</Grid.Column>
-                                <Grid.Column width={ 8 }>
-                                   
-                                    <BtnUI>轉一般使用者</BtnUI>
-                                </Grid.Column>
-                            </>)
-                        }) }
-                    </Grid.Row ></Grid>
+                                        <BtnUI>轉一般使用者</BtnUI>
+                                    </Grid.Column>   </Grid.Row >
+                                </>)
+                            }) }
+                        </Grid>
                 </TabUI.Pane >
             },
 
