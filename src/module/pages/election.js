a43d3df612718712aee65e154b41967334a249e5
Vanessa <10656001@ntub.edu.tw>
2021-10-06 00:00:22 +0800

    調整 - 選舉QA版面

--- src/module/pages/election.js
+++ src/module/pages/election.js
@@ -24,13 +24,13 @@ class Election extends React.Component {
         this.thingRef = React.createRef();
         this.searchRef = React.createRef();
         this.state = {
-            qa: [{ title: "選舉前幾天不能宣傳嗎?為甚麼?會有甚麼後果?", content: "答：投票日不可從事競選或助選活動：1.不可從事競選或助選活動(包含用簡訊或LINE等通訊軟體進行拉票)，違反者，處新臺幣50萬元以上5百萬元以下罰鍰，經制止不聽者，按次連續處罰。2.不可於投票所四周30公尺內喧嚷或干擾勸誘他人投票或不投票，經警衛人員制止後仍繼續為之者，處1年以下有期徒刑、拘役或科新臺幣1萬5千以下罰金。" },
-            { title: "身心障礙者可否由家屬協助投票?", content: "- 答：選舉人如因身心障礙不能自行圈投，而能表示其意思者，得依其請求，由家屬1人在場，依據本人意思，眼同協助或代為圈投；如當事人無法表示其意思，則不適用上開規定。" },
-            { title: "我去投票所排隊的時候，下午四點到了，還能投票嗎?", content: "答：只要您在投票時間截止前到達投票所前排隊領票，仍可投票。" },
-            { title: "不小心圈選於圈選欄之外的選票是否有效?", content: "答：選票圈選欄為3公分＊2.5公分之空白欄位，圈選工具之印文直徑則為1公分，是以選舉人在正常圈選之情況下，圈印印文應不致跨出欄外，但如不小心蓋出欄外，只要所蓋印文蓋於欄格線，仍可辨別圈選何候選人時，仍將被認定為有效票。" },
-            { title: "投票的時候，我不小心蓋錯選票，我該怎麼辦?可以重新領票再投嗎?", content: "答：投票的時候，不小心蓋錯選票，無法重新領票再投。請選舉人於投票時應審慎圈選，若不小心蓋錯選票，仍請投入票匭，請勿撕毀選票，或將選票攜出。若有違反者，撕毀選舉票，將處新臺幣5千 元以上5萬元以下罰鍰；攜出選舉票，將處1年以下有期徒刑、拘役或科新臺幣1萬5千元以下罰金。（總統副總統選舉罷免法第93條第1項、第96條第8項；公職人員選舉罷免法第108條第1項、第110條第8項）" },
-            { title: "甚麼時候會收到選舉公報?", content: "答：各鄉(鎮、市、區)公所會在投票日2日前將選舉公報送達選舉區內各戶。" },
-            { title: "我沒收到投票通知單，怎麼辦?", content: "答：投票通知單由戶政機關依據確定之選舉人名冊填造，送由鄉（鎮、市、區）公所於投票日2日前分送選舉區內各戶。屆時選舉人如未收到，可逕向戶籍地鄉（鎮、市、區）公所洽詢。" },
+            qa: [{ title: "選舉前幾天不能宣傳嗎?為甚麼?會有甚麼後果?", content: "投票日不可從事競選或助選活動：1.不可從事競選或助選活動(包含用簡訊或LINE等通訊軟體進行拉票)，違反者，處新臺幣50萬元以上5百萬元以下罰鍰，經制止不聽者，按次連續處罰。2.不可於投票所四周30公尺內喧嚷或干擾勸誘他人投票或不投票，經警衛人員制止後仍繼續為之者，處1年以下有期徒刑、拘役或科新臺幣1萬5千以下罰金。" },
+            { title: "身心障礙者可否由家屬協助投票?", content: "選舉人如因身心障礙不能自行圈投，而能表示其意思者，得依其請求，由家屬1人在場，依據本人意思，眼同協助或代為圈投；如當事人無法表示其意思，則不適用上開規定。" },
+            { title: "我去投票所排隊的時候，下午四點到了，還能投票嗎?", content: "只要您在投票時間截止前到達投票所前排隊領票，仍可投票。" },
+            { title: "不小心圈選於圈選欄之外的選票是否有效?", content: "選票圈選欄為3公分＊2.5公分之空白欄位，圈選工具之印文直徑則為1公分，是以選舉人在正常圈選之情況下，圈印印文應不致跨出欄外，但如不小心蓋出欄外，只要所蓋印文蓋於欄格線，仍可辨別圈選何候選人時，仍將被認定為有效票。" },
+            { title: "投票的時候，我不小心蓋錯選票，我該怎麼辦?可以重新領票再投嗎?", content: "投票的時候，不小心蓋錯選票，無法重新領票再投。請選舉人於投票時應審慎圈選，若不小心蓋錯選票，仍請投入票匭，請勿撕毀選票，或將選票攜出。若有違反者，撕毀選舉票，將處新臺幣5千 元以上5萬元以下罰鍰；攜出選舉票，將處1年以下有期徒刑、拘役或科新臺幣1萬5千元以下罰金。（總統副總統選舉罷免法第93條第1項、第96條第8項；公職人員選舉罷免法第108條第1項、第110條第8項）" },
+            { title: "甚麼時候會收到選舉公報?", content: "各鄉(鎮、市、區)公所會在投票日2日前將選舉公報送達選舉區內各戶。" },
+            { title: "我沒收到投票通知單，怎麼辦?", content: "投票通知單由戶政機關依據確定之選舉人名冊填造，送由鄉（鎮、市、區）公所於投票日2日前分送選舉區內各戶。屆時選舉人如未收到，可逕向戶籍地鄉（鎮、市、區）公所洽詢。" },
 
             ], activeIndex: 0,
             note: ["公告全國性公民投票案第20案投票日期、投票起、止時間、編號、主文、理由書、政府機關針對公民投票案提出之意見書、公民投票權行使範圍及方式、正反意見支持代表於全國性無線電視頻道發表意見或進行辯論之辦理期間與應遵行之事項等事項", "中選會提醒有意參加公投意見發表會或辯論會者，請在6月4日前申請許可設立辦事處並完成報名", "公告全國性公民投票案第19案投票日期、投票起、止時間、編號、主文、理由書、政府機關針對公民投票案提出之意見書、公民投票權行使範圍及方式、正反意見支持代表於全國性無線電視頻道發表意見或進行辯論之辦理期間與應遵行之事項等事項"]
@@ -99,7 +99,7 @@ class Election extends React.Component {
         const { activeIndex } = this.state
         const panes1 = [
             {
-                menuItem: '投票影片', render: () =>
+                menuItem: '投票流程教學', render: () =>
                     <Tab.Pane>
                         <Embed id='uZp4P70H6E8' /*placeholder='/images/image-16by9.png'*/ source='youtube' />
                     </Tab.Pane>
@@ -129,7 +129,7 @@ class Election extends React.Component {
                     </Tab.Pane>
             },
         ]
-        return (<Pages id={1} class="p-0" pageInfo={[{ content: '選舉報你知', active: true, href: "./#/election" }]} page={ 
+        return (<Pages id={1} class="p-0" pageInfo={[{ content: '選舉報你知', active: true, href: "./#/election" }]} page={
             (<>
                 {/* <Menu secondary>
                     <Menu.Item
@@ -162,93 +162,114 @@ class Election extends React.Component {
                     {/* <!-- Tab Contents --> */}
                     <div id="tab-contents" class="bg-white w-full">
                         <div id="first" class="p-4">
-                                <Grid><Grid.Row>
-                                    <Grid.Column width={16} >
-                                        <div class="p-4 rounded-md" >
-                                            <h1>選舉公告</h1>
-                                            <List animated divided>
-                                                {this.state.note.map((item, index) => {
-                                                    return (<List.Item icon={"bullhorn"} content={item} />)
-                                                })}
-                                            </List>
-                                            <p>
-                                                <button class="bg-red-600 hover:bg-red-800 text-white text-2xl font-semibold rounded-md px-4 py-2 m-2" >看更多</button>
-                                            </p>
-                                        </div>
-                                    </Grid.Column>
-                                </Grid.Row></Grid>
+                            <Grid><Grid.Row>
+                                <Grid.Column width={16} >
+                                    <div class="p-4 px-24 rounded-md" >
+                                        <h1 class="flex justify-center">選舉公告</h1>
+                                        <List class="py-2 text-xl" animated divided>
+                                            {this.state.note.map((item, index) => {
+                                                return (<List.Item class="my-2" icon={"bullhorn"} content={item} />)
+                                            })}
+                                        </List>
+                                        <p>
+                                            <button class="float-right bg-red-600 hover:bg-red-800 text-white text-2xl font-semibold rounded-md px-4 py-2 m-2 mb-4" >看更多</button>
+                                        </p>
+                                    </div>
+                                </Grid.Column>
+                            </Grid.Row></Grid>
                         </div>
 
                         <div id="second" class="hidden p-4">
-                                <Grid><Grid.Row>
-                                    {/* <Grid.Column width={ rate } computer={ rate } mobile={ 16 }>
+                            <Grid><Grid.Row>
+                                {/* <Grid.Column width={ rate } computer={ rate } mobile={ 16 }>
                             
                                             <Button color={ "teal" } variant="secondary" size="lg" disabled >投票要點</Button>
                                         
                                     </Grid.Column> */}
-                                    <Grid.Column width={16}>
-                                        <div class="p-4 rounded-md">
-                                            <Ref innerRef={this.voteRef}><h1 >投票要點</h1></Ref>
-                                            <Tab panes={panes1} />
-                                        </div>
-                                    </Grid.Column>
-                                </Grid.Row></Grid>
+                                <Grid.Column width={16}>
+                                    <div class="p-4 rounded-md">
+                                        <Ref innerRef={this.voteRef}><h1 class="flex justify-center">投票要點</h1></Ref>
+                                        <Tab class="text-xl" panes={panes1} />
+                                    </div>
+                                </Grid.Column>
+                            </Grid.Row></Grid>
                         </div>
 
                         <div id="third" class="hidden p-4">
-                                <Grid><Grid.Row>
-                                    {/* <Grid.Column width={ rate } computer={ rate } mobile={ 16 }>
+                            <Grid><Grid.Row>
+                                {/* <Grid.Column width={ rate } computer={ rate } mobile={ 16 }>
                                 
                                             <Button color={ "teal" } variant="secondary" size="lg" disabled >QA大集合</Button>
                                         
                                     </Grid.Column> */}
 
-                                    <Grid.Column width={16}  >
-                                        <div class="p-4 rounded-md">
-                                            <Ref innerRef={this.QARef}>
-                                                <h1 >QA大集合</h1></Ref>
-                                            <Accordion>
-                                                {this.state.qa.map((item, index) => {
-                                                    return (<>
-                                                        <Accordion.Title
-                                                            active={activeIndex === index}
-                                                            index={index}
-                                                            onClick={this.handleClick}
-                                                        >
-                                                            <Icon name='dropdown' />
-                                                            {item.title}
-                                                        </Accordion.Title>
-                                                        <Accordion.Content active={activeIndex === index}>
-                                                            {item.content}
-                                                        </Accordion.Content>
-                                                    </>)
-                                                })}
+                                <Grid.Column width={16}  >
+                                    <div class="p-4 px-24 rounded-md">
+                                        <Ref innerRef={this.QARef}>
+                                            <h1 class="flex justify-center">QA大集合</h1></Ref>
+                                            {this.state.qa.map((item, index) => {
+                                                return (<>
+                                                <div class="p-6 py-0 bg-white flex items-center space-x-6 rounded-lg hover:scale-105 transition transform duration-500">
+                                                    <div  class=" h-20 w-20">
+                                                        <h1  class="text-red-600 text-9x1">Q</h1>
+                                                        {/* <img src={Q}></img> */}
+                                                    </div>
+                                                    <div>
+                                                        <h1 class="text-x6 font-bold text-gray-700">{item.title}</h1>                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="p-6 bg-white flex items-center space-x-6 rounded-lg">
+                                                    <div  class=" h-20 w-20">
+                                                        <h1  class="text-gray-700 text-9x1">A</h1>
+                                                        {/* <img src={Q}></img> */}
+                                                    </div>
+                                                    <div>
+                                                        <p class="text-2x1 text-gray-700 mb-2">{item.content}</p>
+                                                    </div>
+                                                </div>
+                                        {/* <Accordion>
+                                            
+                                                    <Accordion.Title
+                                                        active={activeIndex === index}
+                                                        index={index}
+                                                        onClick={this.handleClick}
+                                                    >
+                                                        <Icon name='dropdown' />
+                                                        {item.title}
+                                                    </Accordion.Title>
+                                                    <Accordion.Content active={activeIndex === index}>
+                                                        {item.content}
+                                                    </Accordion.Content>
+                                               
+                                            
 
 
-                                            </Accordion>
-                                        </div>
-                                    </Grid.Column>
-                                </Grid.Row></Grid>
+                                        </Accordion> */}
+                                        </>)
+                                        })}
+                                    </div>
+                                </Grid.Column>
+                            </Grid.Row></Grid>
                         </div>
 
                         <div id="fourth" class="hidden p-4">
-                                <Grid><Grid.Row>
-                                    {/* <Grid.Column width={ rate } computer={ rate } mobile={ 16 }>
+                            <Grid><Grid.Row>
+                                {/* <Grid.Column width={ rate } computer={ rate } mobile={ 16 }>
                                         <Button color={ "teal" } variant="secondary" size="lg" disabled >選舉大事紀</Button>
                                     </Grid.Column> */}
-                                    <Grid.Column width={16} >
-                                        <div class="p-4 rounded-md">
-                                            <Ref innerRef={this.thingRef}><h1 >民主大事紀</h1></Ref>
-                                            <p>這裡是民主大事紀,記錄著台灣民主有關發生的大事件。 </p>
-                                            <Image src={time1} size='large' centered />
-                                            <Image src={time2} size='large' centered />
-                                            <Image src={time3} size='large' centered />
-                                            <Image src={time4} size='large' centered />
-                                            <Image src={time5} size='large' centered />
-                                            <Image src={time6} size='large' centered />
-                                            <Image src={time7} size='large' centered />
+                                <Grid.Column width={16} >
+                                    <div class="p-4 rounded-md">
+                                        <Ref innerRef={this.thingRef}><h1 class="flex justify-center">民主大事紀</h1></Ref>
+                                        <p class="flex justify-center">這裡是民主大事紀,記錄著台灣民主有關發生的大事件。 </p>
+                                        <Image src={time1} size='large' centered />
+                                        <Image src={time2} size='large' centered />
+                                        <Image src={time3} size='large' centered />
+                                        <Image src={time4} size='large' centered />
+                                        <Image src={time5} size='large' centered />
+                                        <Image src={time6} size='large' centered />
+                                        <Image src={time7} size='large' centered />
 
-                                            {/* <Table celled>
+                                        {/* <Table celled>
                                     <Table.Header>
                                         <Table.Row>
                                             <Table.HeaderCell>屆別</Table.HeaderCell>
@@ -386,7 +407,7 @@ class Election extends React.Component {
                                         </Table.Row>
                                     </Table.Body> */}
 
-                                            {/* <Table.Footer>
+                                        {/* <Table.Footer>
                                         <Table.Row>
                                             <Table.HeaderCell colSpan='3'>
                                                 <Menu floated='right' pagination>
@@ -406,9 +427,9 @@ class Election extends React.Component {
                                     </Table.Footer> 
 
                                 </Table>*/}
-                                        </div>
-                                    </Grid.Column>
-                                </Grid.Row></Grid>
+                                    </div>
+                                </Grid.Column>
+                            </Grid.Row></Grid>
                         </div>
 
                     </div>
